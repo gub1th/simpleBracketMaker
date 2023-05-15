21f937bc5854980aad5ab768676217f4c21ac582
gub1th <danielfgunawan1@gmail.com>
2023-05-14 22:52:25 -0400

    created basic form input

--- simpleBracketMaker/src/App.jsx
+++ simpleBracketMaker/src/App.jsx
@@ -1,5 +1,42 @@
+import React, { useState } from 'react';
+
 function App() {
 
+  const [formData, setFormData] = useState({
+    teamName : "",
+    member1 : "",
+    member2 : ""
+  })
+
+  const handleInputChange = (event) => {
+    const {id, value} = event.target;
+    setFormData((prevData)=> ({
+      ...prevData,
+      [id]: value,
+    }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  }
+
+  return (
+    <>
+      <h1>Welcome to the SimpleBracketMaker!</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="teamName">Team Name</label><br/>
+        <input type="text" id="teamName" onChange={handleInputChange}/><br/>
+        <label htmlFor="member1">Member 1</label><br/>
+        <input type="text" id="member1" onChange={handleInputChange}/><br/>
+        <label htmlFor="member2">Member 2</label><br/>
+        <input type="text" id="member2" onChange={handleInputChange}/><br/>
+        
+        <button type="submit">Add Team</button>
+      </form>
+    </>
+  )
 }
 
 
