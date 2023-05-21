8eb86386aae6c97e08fb303af232310c191c9dd8
Daniel Gunawan <dgunawan@andrew.cmu.edu>
2023-05-21 14:50:41 -0400

    made formComponent

--- simpleBracketMaker/src/HomePage.jsx
+++ simpleBracketMaker/src/HomePage.jsx
@@ -1,28 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import FormComponent from './components/FormComponent';
 
 function HomePage() {
-
-    //for initializing/resetting
-    const initialFormData = {
-      teamName : "",
-      member1 : "",
-      member2 : ""
-    };
-  
-    //for the form
-    const [formData, setFormData] = useState(initialFormData);
-  
     //actual team(s) data
     const [teamData, setTeamData] = useState([]);
   
-    //for error handling
-    const [error, setError] = useState("");
-  
     //for error handling of randomize
     const [error2, setError2] = useState("");
-  
-    //for successfully creating team
-    const [success, setSuccess] = useState("");
   
     //for the rounds
     const [rounds, setRounds] = useState([]);
@@ -43,43 +27,6 @@ function HomePage() {
         tempRounds.push(tempRound);
       }
       return tempRounds;
-    }
-  
-    const handleInputChange = (event) => {
-      const {id, value} = event.target;
-      setFormData((prevData)=> ({
-        ...prevData,
-        [id]: value,
-      }))
-    };
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-  
-      const teamNameInput = formData.teamName.trim();
-      const member1Input = formData.member1.trim();
-      const member2Input = formData.member2.trim();
-  
-      if (teamNameInput === "" || member1Input === "" || member2Input === "") {
-        setError("Please fill in all the fields");
-        return;
-      }
-      if (member1Input !== member1Input.replace(/[^a-zA-Z]/g, "") || member2Input.trim() !== member2Input.replace(/[^a-zA-Z]/g, "")) {
-        setError("Member names should only contain alphabetic characters.");
-        return;
-      }
-      if (teamData.some((team) => team.teamName === teamNameInput)) {
-        setError("Team name already exists.");
-        return;
-      }
-      setTeamData((prevTeamData) => [...prevTeamData, formData]);
-      //this not working? like i added team and immediately press submit again
-      setFormData(initialFormData);
-      setError("");
-      setSuccess("Team successfully added.");
-  
-      //clear success message
-      setTimeout(() => {setSuccess('');}, 3000);
     }
   
     const randomizeTeams = () => {
@@ -135,22 +82,12 @@ function HomePage() {
   
     return (
       <>
-        {/* <NavigationBar/> */}
         <h1>Welcome to the SimpleBracketMaker!</h1>
   
-        <div>
-          <form onSubmit={handleSubmit}>
-            <label className="formLabel" htmlFor="teamName">Team Name</label><br/>
-            <input className="formInput" type="text" id="teamName" value={formData.teamName} onChange={handleInputChange}/><br/>
-            <label className="formLabel" htmlFor="member1">Member 1</label><br/>
-            <input className="formInput" type="text" id="member1" value={formData.member1} onChange={handleInputChange}/><br/>
-            <label className="formLabel" htmlFor="member2">Member 2</label><br/>
-            <input className="formInput" type="text" id="member2" value={formData.member2} onChange={handleInputChange}/><br/>
-            <button type="submit">Add Team</button>
-          </form>
-          {error && (<div className="alert alert-danger" role="alert">{error}</div>)}
-          {success && (<div className="alert alert-success" role="alert">{success}</div>)}
-        </div>
+        <FormComponent
+            teamData = {teamData}
+            setTeamData = {setTeamData}
+        />
         <div>
           <ul className='listGroup'>
               {teamData.map(
---
+++ simpleBracketMaker/src/components/FormComponent.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+
+function FormComponent ({teamData, setTeamData}) {
+    //for initializing/resetting
+    const initialFormData = {
+        teamName : "",
+        member1 : "",
+        member2 : ""
+    };
+
+    //for the form data
+    const [formData, setFormData] = useState(initialFormData);
+
+    //for error handling of form
+    const [formError, setFormError] = useState("");
+
+    //for successfully creating team
+    const [formSuccess, setFormSuccess] = useState("");
+
+    const handleInputChange = (event) => {
+        const {id, value} = event.target;
+        setFormData((prevData)=> ({
+          ...prevData,
+          [id]: value,
+        }))
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const teamNameInput = formData.teamName.trim();
+        const member1Input = formData.member1.trim();
+        const member2Input = formData.member2.trim();
+
+        if (teamNameInput === "" || member1Input === "" || member2Input === "") {
+        setFormError("Please fill in all the fields");
+        return;
+        }
+        if (member1Input !== member1Input.replace(/[^a-zA-Z]/g, "") || member2Input.trim() !== member2Input.replace(/[^a-zA-Z]/g, "")) {
+        setFormError("Member names should only contain alphabetic characters.");
+        return;
+        }
+        if (teamData.some((team) => team.teamName === teamNameInput)) {
+        setFormError("Team name already exists.");
+        return;
+        }
+        setTeamData((prevTeamData) => [...prevTeamData, formData]);
+        setFormData(initialFormData);
+        setFormError("");
+        setFormSuccess("Team successfully added.");
+
+        //clear success message
+        setTimeout(() => {setFormSuccess('');}, 3000);
+    }
+
+    return (
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label className="formLabel" htmlFor="teamName">Team Name</label><br/>
+            <input className="formInput" type="text" id="teamName" value={formData.teamName} onChange={handleInputChange}/><br/>
+            <label className="formLabel" htmlFor="member1">Member 1</label><br/>
+            <input className="formInput" type="text" id="member1" value={formData.member1} onChange={handleInputChange}/><br/>
+            <label className="formLabel" htmlFor="member2">Member 2</label><br/>
+            <input className="formInput" type="text" id="member2" value={formData.member2} onChange={handleInputChange}/><br/>
+            <button type="submit">Add Team</button>
+          </form>
+          {formError && (<div className="alert alert-danger" role="alert">{formError}</div>)}
+          {formSuccess && (<div className="alert alert-success" role="alert">{formSuccess}</div>)}
+        </div>
+    )
+}
+
+export default FormComponent;
